71b909a19dc736a8de4a0eb92041bc6e659ccbcd
hoanganhk52 <hoanganhctm7@gmail.com>
2020-03-04 21:49:28 +0700

    add api brewer detail

--- api_data.js
+++ api_data.js
@@ -26,5 +26,33 @@ define({ "api": [
         }
       ]
     }
+  },
+  {
+    "type": "get",
+    "url": "api/v1/brewer/:id",
+    "title": "Brewer detail",
+    "name": "Brewer_detail",
+    "group": "Brewer",
+    "success": {
+      "examples": [
+        {
+          "title": "Success-Response:",
+          "content": "HTTP/1.1 200 OK\n{\n     \"status\": \"OK\",\n     \"message\": null,\n     \"data\": [\n         {\n             \"id\": 1,\n             \"name\": \"旭酒造\",\n             \"prefecture\": \"北海道\",\n             \"city\": \"岩国市\",\n             \"address\": \"〒742-0422 山口県岩国市周東町獺越2167-4\"\n             \"description\": \"精米歩合50%以下の純米大吟醸「獺祭」のシリーズのみを造っているのが特徴。また杜氏を使わず、徹底的に数値化したデータを元に、醸造管理を行なって日本酒造りを行なっている。\",\n             \"images\": [\n                 \"https://sakeproduction.s3.ap-northeast-1.amazonaws.com/uploads/products/4cpIUkQjHHX6YyCFjyfYcX7svw2ouTyqw4FKaUjL.png\",\n                 \"https://sakeproduction.s3.ap-northeast-1.amazonaws.com/uploads/products/4cpIUkQjHHX6YyCFjyfYcX7svw2ouTyqw4FKaUjL.png\",\n                 \"https://sakeproduction.s3.ap-northeast-1.amazonaws.com/uploads/products/4cpIUkQjHHX6YyCFjyfYcX7svw2ouTyqw4FKaUjL.png\",\n                 \"https://sakeproduction.s3.ap-northeast-1.amazonaws.com/uploads/products/4cpIUkQjHHX6YyCFjyfYcX7svw2ouTyqw4FKaUjL.png\",\n                 \"https://sakeproduction.s3.ap-northeast-1.amazonaws.com/uploads/products/4cpIUkQjHHX6YyCFjyfYcX7svw2ouTyqw4FKaUjL.png\"\n             ]\n             \"brands\": [\n                 {\n                     \"id\": 521,\n                     \"name\": \"南部美人　純米吟醸\",\n                     \"image\": \"https://sakeproduction.s3.ap-northeast-1.amazonaws.com/uploads/products/4cpIUkQjHHX6YyCFjyfYcX7svw2ouTyqw4FKaUjL.png\",\n                 },\n                 {\n                     \"id\": 521,\n                     \"name\": \"南部美人　純米吟醸\",\n                     \"image\": \"https://sakeproduction.s3.ap-northeast-1.amazonaws.com/uploads/products/4cpIUkQjHHX6YyCFjyfYcX7svw2ouTyqw4FKaUjL.png\",\n                 },\n                 ...\n             ]\n         }\n     ]\n}",
+          "type": "json"
+        }
+      ]
+    },
+    "version": "0.0.0",
+    "filename": "doc/brewer.js",
+    "groupTitle": "Brewer",
+    "header": {
+      "examples": [
+        {
+          "title": "Header-Request:",
+          "content": "X-Requested-With: XMLHttpRequest\nContent-Type: application/json\nAuthorization: Bearer {{access_token}}",
+          "type": "String"
+        }
+      ]
+    }
   }
 ] });
--- api_project.js
+++ api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2020-02-27T15:19:42.583Z",
+    "time": "2020-03-04T14:48:26.231Z",
     "url": "http://apidocjs.com",
     "version": "0.20.0"
   }
